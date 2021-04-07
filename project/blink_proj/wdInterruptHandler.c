1bfe83b7564a38e4127571a2bde77d6ea60cdfdc
mjavila4 <60335477+mjavila4@users.noreply.github.com>
2021-04-07 14:07:32 -0600

    Update wdInterruptHandler.c
    interrupts added for dimState()

--- project/blink_proj/wdInterruptHandler.c
+++ project/blink_proj/wdInterruptHandler.c
@@ -4,12 +4,16 @@
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
+  static char dim_count = 0;
   
   if(++blink_count == 125){
-
     state_advance();
     blink_count = 0;
-
+  }
+  
+  if(++dim_count == 2){
+    dimState();
+    dim_count = 0;
   }
   
 }
