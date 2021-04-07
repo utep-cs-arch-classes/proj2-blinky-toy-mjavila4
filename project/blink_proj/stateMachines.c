5e60adcc5d5e82cab0bd1e98d9d0d3ea0f2baf3a
Mark-Anthony <mjavila4@miners.utep.edu>
2021-04-07 00:43:33 -0600

    Testing dimming

--- project/blink_proj/stateMachines.c
+++ project/blink_proj/stateMachines.c
@@ -2,47 +2,65 @@
 #include "stateMachines.h"
 #include "led.h"
 
-char toggle_red()		/* always toggle! */
-{
-  static char state = 0;
+static char toggleRedState = 0;
 
-  switch (state) {
+char toggleRed(){
+
+  switch(toggleRedState) {
   case 0:
     red_on = 1;
-    state = 1;
+    toggleRedState = 1;
     break;
   case 1:
     red_on = 0;
+    toggleRedState = 0;
+    break;
+  }
+  return 1;
+}
+
+char toggleGreen(){
+
+  static char state = 1;
+
+  switch(state){
+  case 0:
+    green_on = 1;
+    state = 0;
+    break;
+  case 1:
+    green_on = 1;
     state = 0;
     break;
   }
-  return 1;			/* always changes an led */
-}
-
-char toggle_green()	/* only toggle green if red is on!  */
-{
-  char changed = 0;
-  if (red_on) {
-    green_on ^= 1;
-    changed = 1;
-  }
-  return changed;
+  return 1;
 }
 
 
-void state_advance()		/* alternate between toggling red & green */
-{
-  char changed = 0;  
+void state_advance(){
 
-  static enum {R=0, G=1} color = G;
-  switch (color) {
-  case R: changed = toggle_red(); color = G; break;
-  case G: changed = toggle_green(); color = R; break;
+  static char state = 0;
+
+  switch(state){
+  case 0:
+    green_on = 1;
+    state = 1;
+    break;
+  case 1:
+    green_on = 0;
+    state = 0;
+    break;
   }
 
-  led_changed = changed;
+  led_changed = 1;
   led_update();
-}
+  
+} 
+
+
+
+
+
 
 
 
