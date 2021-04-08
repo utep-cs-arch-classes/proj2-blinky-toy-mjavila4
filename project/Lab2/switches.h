76a0ce59164fb8f0c05b7826ddfe9c14d75a6a98
mjavila4 <60335477+mjavila4@users.noreply.github.com>
2021-04-08 10:19:30 -0600

    Update switches.h

--- project/Lab2/switches.h
+++ project/Lab2/switches.h
@@ -5,11 +5,11 @@
 #define SW2 BIT1
 #define SW3 BIT2
 #define SW4 BIT3
-#define SWITCHES (SW1 | SW1 | SW1 | SW1)
+#define SWITCHES (SW1 | SW2 | SW3 | SW4)
 
 void switch_init();
 void switch_interrupt_handler();
 
-extern char switch_state_down, switch_state_changed; /* effectively boolean */
+extern char switch_state_down1, switch_state_down2, switch_state_down3, switch_state_down4, switch_state_changed;
 
 #endif // included
