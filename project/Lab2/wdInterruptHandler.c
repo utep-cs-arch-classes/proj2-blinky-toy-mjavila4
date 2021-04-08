64b7aefff61b23d4cb5803139ce4e2f51f609587
mjavila4 <60335477+mjavila4@users.noreply.github.com>
2021-04-08 09:19:14 -0600

    Add files via upload

---
+++ project/Lab2/wdInterruptHandler.c
@@ -0,0 +1,19 @@
+#include <msp430.h>
+#include "stateMachines.h"
+
+void
+__interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
+  static char blink_count = 0;
+  static char dim_count = 0;
+  
+  if(++blink_count == 125){
+    state_advance();
+    blink_count = 0;
+  }
+  
+  if(++dim_count == 1){
+    dimState();
+    dim_count = 0;
+  }
+  
+}
