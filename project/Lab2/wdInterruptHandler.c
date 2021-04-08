d8d4e790b69ac63b0ee1bc31e6b92b6258a29b8f
Mark-Anthony <mjavila4@miners.utep.edu>
2021-04-08 09:49:17 -0600

    Test

--- project/Lab2/wdInterruptHandler.c
+++ project/Lab2/wdInterruptHandler.c
@@ -2,7 +2,7 @@
 #include "stateMachines.h"
 
 void
-__interrupt_vecTime(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
+__interrupt_vecTime(WDT_VECTOR) WDT(){	// 250 interrupts/sec //
   static char blink_count = 0;
   static char dim_count = 0;
   
@@ -20,8 +20,8 @@ __interrupt_vecTime(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
 
 /*void
 __interrupt_vecSwitch(PORT1_VECTOR) Port_1(){
-  if (P1IFG & SWITCHES) {	      /* did a button cause this interrupt? */
-    P1IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
-    switch_interrupt_handler();	/* single handler for all switches */
+  if (P1IFG & SWITCHES) {	      // did a button cause this interrupt? //
+    P1IFG &= ~SWITCHES;		      // clear pending sw interrupts //
+    switch_interrupt_handler();	      // single handler for all switches //
   }
 }*/
