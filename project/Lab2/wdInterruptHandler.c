622d1c15b4ac46fa1589473d5028cfeedaa844de
mjavila4 <60335477+mjavila4@users.noreply.github.com>
2021-04-08 09:37:57 -0600

    Update wdInterruptHandler.c

--- project/Lab2/wdInterruptHandler.c
+++ project/Lab2/wdInterruptHandler.c
@@ -17,3 +17,11 @@ __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   }
   
 }
+
+void
+__interrupt_vec(PORT1_VECTOR) Port_1(){
+  if (P1IFG & SWITCHES) {	      /* did a button cause this interrupt? */
+    P1IFG &= ~SWITCHES;		      /* clear pending sw interrupts */
+    switch_interrupt_handler();	/* single handler for all switches */
+  }
+}
