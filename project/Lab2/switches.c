21cc5c4b824695d1eaf64007dfe8fc73acf4fc82
mjavila4 <60335477+mjavila4@users.noreply.github.com>
2021-04-08 10:18:20 -0600

    Update switches.c

--- project/Lab2/switches.c
+++ project/Lab2/switches.c
@@ -29,7 +29,10 @@ void
 switch_interrupt_handler()
 {
   char p1val = switch_update_interrupt_sense();
-  switch_state_down = (p1val & SW1) ? 0 : 1; /* 0 when SW1 is up */
+  switch_state_down1 = (p1val & SW1) ? 0 : 1;
+  switch_state_down2 = (p1val & SW2) ? 0 : 1;
+  switch_state_down3 = (p1val & SW3) ? 0 : 1;
+  switch_state_down4 = (p1val & SW4) ? 0 : 1;
   switch_state_changed = 1;
   led_update();
 }
