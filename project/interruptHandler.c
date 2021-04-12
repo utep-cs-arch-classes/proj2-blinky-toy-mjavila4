1c72ce8a58247287a6dad0e2bed08e232a0b5bf1
mjavila4 <60335477+mjavila4@users.noreply.github.com>
2021-04-11 22:36:23 -0600

    Condensing code

--- project/interruptHandler.c
+++ project/interruptHandler.c
@@ -4,27 +4,17 @@
 #include "dimmer.h"
 
 void __interrupt_vec(WDT_VECTOR) WDT(){
-
   static char blinkCount = 0;
-
   if(++blinkCount == 125){
-
     stateAdvance();
     blinkCount = 0;
-
   }
-
   stateDim();
-
 }
 
 void __interrupt_vec(PORT2_VECTOR) Port_2(){
-
   if(P2IFG & SWITCHES){
-
     P2IFG &= ~SWITCHES;
     switchInterruptHandler();
-    
   }
-
 }
