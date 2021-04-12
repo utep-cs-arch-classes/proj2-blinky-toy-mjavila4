e6f0f51684e70f016dc7a2fc6e3d8ce92d0dda2e
mjavila4 <60335477+mjavila4@users.noreply.github.com>
2021-04-11 22:40:01 -0600

    Condensing code

--- project/led.c
+++ project/led.c
@@ -9,25 +9,17 @@ unsigned char ledChanged = 0;
 static char redVal[] = {0, LED_RED}, greenVal[] = {0, LED_GREEN};
 
 void ledInit(){
-
   P1DIR |= LEDS;
   switchStateChanged = 1;
   ledUpdate();
-
 }
 
 void ledUpdate(){
-
   if(switchStateChanged || ledChanged){
-    
     char ledFlags = redVal[redOn] | greenVal[greenOn];
-
     P1OUT &= (0xff - LEDS) | ledFlags;
     P1OUT |= ledFlags;
-
   }
-
   switchStateChanged = 0;
   ledChanged = 0;
-
 }
