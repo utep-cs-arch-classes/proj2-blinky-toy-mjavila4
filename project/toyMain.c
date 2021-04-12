03d59114ff159f4bacbac071b8f2272af8d45c12
mjavila4 <60335477+mjavila4@users.noreply.github.com>
2021-04-11 22:01:58 -0600

    Update toyMain.c
    Added buzzer initializer

--- project/toyMain.c
+++ project/toyMain.c
@@ -3,12 +3,14 @@
 #include "led.h"
 #include "switches.h"
 #include "dimmer.h"
+#include "buzzer.h"
 
 void main(void){
 
   configureClocks();
   switchInit();
   ledInit();
+  buzzer_init();
   
   enableWDTInterrupts();
 
